5519add20ecc62ecab7ae968c9a78eb769959cb4
serafim.ciobanu <ciserafim2003@gmail.com>
2023-07-18 00:51:36 +0300

    Small fix for parallax effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ function init() {
         Object.assign(document.documentElement, {
             style: `
             --move-x: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
-            --move-y: ${(e.clientY - window.innerHeight / 2) * -0.01}deg;
+            --move-y: ${(e.clientY - window.innerHeight / 2) * 0.01}deg;
             `
         })
     })
