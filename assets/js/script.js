4b94ff2999a8e838b270c6d0023794f956909745
serafim.ciobanu <ciserafim2003@gmail.com>
2023-07-17 23:51:54 +0300

    Site being transformed on change of cursor position

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,8 @@ function init() {
     document.addEventListener("mousemove", e => {
         Object.assign(document.documentElement, {
             style: `
-            --move-x: ${e.clientX};
-            --move-y: ${e.clientY};
+            --move-x: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
+            --move-y: ${(e.clientY - window.innerHeight / 2) * -0.01}deg;
             `
         })
     })
